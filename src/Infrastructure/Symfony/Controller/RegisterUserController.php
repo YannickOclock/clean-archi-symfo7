9b0778b47b7b67eacd7fa6f5406d214b340836d5
YannickOclock <yannick.kuhn@oclock.io>
2024-03-23 23:52:00 +0100

    Login/Register - ok fonctionnel

---
+++ src/Infrastructure/Symfony/Controller/RegisterUserController.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Symfony\Controller;
+
+use App\Domain\User\UseCase\Register\RegisterUserRequest;
+use App\Domain\User\UseCase\Register\RegisterUserUseCaseInterface;
+use App\Infrastructure\Symfony\Service\RegisterUserDataValidator;
+use App\Infrastructure\Symfony\View\RegisterHtmlView;
+use App\Presentation\User\RegisterUserHtmlPresenter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
+
+#[Route('/register', name: 'app_register')]
+final class RegisterUserController extends AbstractController
+{
+    private RegisterHtmlView $registerView;
+    private RegisterUserUseCaseInterface $registerUseCase;
+    private RegisterUserHtmlPresenter $presenter;
+    private RegisterUserDataValidator $registerUserDataValidator;
+
+    public function __construct(
+        RegisterHtmlView $registerView,
+        RegisterUserUseCaseInterface $registerUseCase,
+        RegisterUserHtmlPresenter $presenter,
+        RegisterUserDataValidator $registerUserDataValidator
+    ) {
+        $this->registerView = $registerView;
+        $this->registerUseCase = $registerUseCase;
+        $this->presenter = $presenter;
+        $this->registerUserDataValidator = $registerUserDataValidator;
+    }
+
+    public function __invoke(Request $request, RegisterUserRequest $registerUserRequest): Response
+    {
+
+        $registerUserData = $request->request->all();
+        $isPosted = $registerUserData['register_user']['isPosted'] ?? null;
+        if ($isPosted !== null) {
+            $registerUserRequest->violations = $this->registerUserDataValidator->gerErrors($registerUserData['register_user'] ?? null);
+            $registerUserRequest->isPosted = (bool)(int) $isPosted;
+            $registerUserRequest->email = $registerUserData['register_user']['email'] ?? null;
+            $registerUserRequest->password = $registerUserData['register_user']['password'] ?? null;
+            $registerUserRequest->firstName = $registerUserData['register_user']['firstName'] ?? null;
+            $registerUserRequest->lastName = $registerUserData['register_user']['lastName'] ?? null;
+            $registerUserRequest->id = Uuid::v4()->toRfc4122();
+        }
+
+        $this->registerUseCase->execute($registerUserRequest, $this->presenter);
+
+        return $this->registerView->generateView(
+            $registerUserRequest,
+            $this->presenter->viewModel()
+        );
+    }
+}
--- src/Infrastructure/Symfony/Repository/UserRepository.php
+++ src/Infrastructure/Symfony/Repository/UserRepository.php
@@ -2,12 +2,16 @@
 
 namespace App\Infrastructure\Symfony\Repository;
 
+use App\Domain\User\Entity\User as DomainUser;
+use App\Domain\User\Repository\UserRepositoryInterface;
 use App\Infrastructure\Symfony\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -17,9 +21,12 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface, UserRepositoryInterface
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+        ManagerRegistry $registry,
+    )
     {
         parent::__construct($registry, User::class);
     }
@@ -39,4 +46,35 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     }
 
 
+    public function add(DomainUser $domainUser): void
+    {
+        $user = new User();
+        $user->setId(Uuid::fromRfc4122($domainUser->getId()));
+        $user->setEmail($domainUser->getEmail());
+        $user->setFirstName($domainUser->getFirstName());
+        $user->setLastName($domainUser->getLastName());
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $domainUser->getPassword()
+        );
+        $user->setPassword($hashedPassword);
+
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+
+    public function remove(DomainUser $domainUser, bool $flush = false): void
+    {
+        $user = $this->findOneBy(['email' => $domainUser->getEmail()]);
+        if (!$user) {
+            return;
+        }
+
+        $this->getEntityManager()->remove($user);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 }
