53f915535bc2694aea95f505d67fa770c24ffa79
YannickOclock <yannick.kuhn@oclock.io>
2024-03-23 23:02:58 +0100

    Création de l'entité User + réajustement Kernel

--- src/Infrastructure/Symfony/Controller/HomeController.php
+++ src/Infrastructure/Symfony/Controller/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Infrastructure\Symfony\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
